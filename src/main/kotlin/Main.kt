99a0a95e0f7025955b59d29afce5a80daf3be00e
sp-watson <steve.watson@digital,justice.gov.uk>
2021-12-17 14:40:56 +0000

    NN-3881: Version that works for recall in dev

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,45 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.net.HttpURLConnection
+import java.net.URL
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    println("Attempting to recall")
+
+    val movementTimeString = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+    val offenderNo = "[e.g. A1234AA]"
+    val token = """
+        [Token here]
+    """.trimIndent()
+    val data = """
+        {
+          "prisonId": "MDI",
+          "movementReasonCode": "24",
+          "recallTime": "$movementTimeString",
+          "imprisonmentStatus": "CUR_ORA",
+          "youthOffender": false
+        }
+    """
+    val conn = URL("https://api-dev.prison.service.justice.gov.uk/api/offenders/$offenderNo/recall").openConnection() as HttpURLConnection
+    conn.requestMethod = "PUT"
+    conn.doOutput = true
+    conn.setRequestProperty("Content-Type", "application/json")
+    conn.setRequestProperty("Authorization", "Bearer $token")
+    conn.outputStream.write(data.toByteArray(Charsets.UTF_8))
+    try {
+        val lines = conn.inputStream.use {
+            it.bufferedReader().readLines()
+        }
+        lines.forEach{
+            println("Line: $it")
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        val errorInfo = conn.errorStream.use {
+            it.bufferedReader().readLines()
+        }
+        errorInfo.forEach{
+            println("Error Line: $it")
+        }
+    }
 }

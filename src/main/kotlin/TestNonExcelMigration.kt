8fef49b4c87c52aa5b4be4b7fbfed9c3ac2f7aa0
sp-watson <steve.watson@digital,justice.gov.uk>
2022-05-25 10:52:15 +0100

    NN-4127: Entry point for testing migrations without XLS

---
+++ src/main/kotlin/TestNonExcelMigration.kt
@@ -0,0 +1,28 @@
+import gateways.RestrictedPatientsApi
+import fileaccess.PerOffenderFileOutput
+import fileaccess.SuccessfulOffenderMigrations
+import rpmigration.MigrateOffender
+import rpmigration.SingleTestMigration
+import rpmigration.SingleTestMigrationRequestEvent
+
+fun main() {
+    // ====== MUST DO ON VPN ========
+    val config: Config = PreProd()
+
+    val archiveSubDirectory = "singlemigrationarchive"
+    val existingSuccessfulMigrationsFileName = "SUCCESSFUL_MIGRATIONS.txt"
+    val existingAlreadyInPrisonFileName = "ALREADY_IN_PRISON.txt"
+
+    val offenderNo = "A0666ER" // Ultimate test = A0666ER
+    val hospitalNomsId = "ASHWTH"
+
+    val successfulOffenderMigrations = SuccessfulOffenderMigrations(config.resultsBaseDirectory, existingSuccessfulMigrationsFileName, existingAlreadyInPrisonFileName)
+    val rpApi = RestrictedPatientsApi(config.restrictedPatientsApiRootUrl, config.token)
+
+    val migrationCommand = SingleTestMigration(
+        PerOffenderFileOutput(config.resultsBaseDirectory, archiveSubDirectory),
+        MigrateOffender(successfulOffenderMigrations, rpApi)
+    )
+
+    migrationCommand.handle(SingleTestMigrationRequestEvent(offenderNo, hospitalNomsId))
+}
